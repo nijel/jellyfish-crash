bf0ce6b7b94d8efd5d7f6ecd3c6437a2412cd53d
Michal Čihař <michal@cihar.com>
2021-10-11 15:24:55 +0200

    Add more tests

--- test.py
+++ test.py
@@ -3,4 +3,12 @@ from jellyfish import damerau_levenshtein_distance
 print("test1", damerau_levenshtein_distance("a", "b"))
 print("test2", damerau_levenshtein_distance("a", "a"))
 print("test3", damerau_levenshtein_distance("NICHOLASŸ", "NICHOLAS"))
-print("test4", damerau_levenshtein_distance("a" * 200000, "b" * 200000))
+try:
+    print("test4", damerau_levenshtein_distance("a" * 200000, "b" * 200000))
+except MemoryError:
+    print("test4 MemoryError")
+try:
+    print("test5", damerau_levenshtein_distance("a" * 100000, "b" * 100000))
+except MemoryError:
+    print("test5 MemoryError")
+print("test6", damerau_levenshtein_distance("NICHOLASŸ", "NICHOLAS"))
